268e96601553dae88531abc229353ba2454669b2
Dayana Silva <61297570+dayanasilvacaffarel@users.noreply.github.com>
2023-04-07 22:32:49 -0300

    Add files via upload

---
+++ ToDo-App/scripts/pruebas.js
@@ -0,0 +1,9 @@
+let nombre = "pedro"
+
+function mostrarNombre(nombre){
+    if (nombre !== undefined){
+        nombre = "tu nombre es" + " "+ nombre;
+    }
+}
+mostrarNombre()
+console.log(nombre);
